6cbc3706842072015eb5a035a19ab912298a40ee
vladyslavpalamarchuk <v.palamarchuk9@gmail.com>
2024-10-31 16:18:42 +0200

    initial commit

---
+++ src/main/java/com/vladyslavpalamarchuk/trackmycoin/TrackmycoinApplication.java
@@ -0,0 +1,13 @@
+package com.vladyslavpalamarchuk.trackmycoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmycoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmycoinApplication.class, args);
+	}
+
+}
